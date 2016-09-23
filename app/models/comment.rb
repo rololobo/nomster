ba1cf7c870aab5f67de90007b50bef5fc62e38e6
Rosie <ariel.highsmith@gmail.com>
2016-09-23 09:59:19 -0700

    Send email when new comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
+  
   RATINGS = {
     'one star' => '1_star',
     'two stars' => '2_stars',
@@ -13,4 +15,7 @@ class Comment < ActiveRecord::Base
     RATINGS.invert[self.rating]
   end
   
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver
+  end
 end
