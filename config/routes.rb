96ee16bafccf18a1c6eec3d166a89eb375641c9a
Rosie <ariel.highsmith@gmail.com>
2016-09-24 13:39:49 -0700

    Add User dashboard page route and link

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
     resources :comments, only: :create
     resources :photos, only: :create
   end
+  resources :users, only: :show
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
