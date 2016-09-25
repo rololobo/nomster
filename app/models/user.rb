62ee3029d17f184cb9e0739e315fc7a35a33d862
Rosie <ariel.highsmith@gmail.com>
2016-09-25 12:09:45 -0700

    place dependent destroy on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :places
+  has_many :places, dependent: :destroy 
   has_many :comments
   validates :email, uniqueness: true
 end
