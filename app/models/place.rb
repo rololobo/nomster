b7b08e7d1811231e0d7d4ba6e1f52ff716795f20
Rosie <ariel.highsmith@gmail.com>
2016-09-25 21:02:45 -0700

    Adjsut dependent destroy so only comments and places are linked

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,6 +6,6 @@ class Place < ActiveRecord::Base
   length: { minimum: 3 }
   validates :address, presence: true, length: { minimum: 6}
   validates :description, presence: true, length: { maximum: 500}
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :photos
 end
