d699e9be377ddba037198b935657f61d02faa020
Rosie <ariel.highsmith@gmail.com>
2016-09-29 09:09:05 -0700

    Redirect to place after editing and creating new instead of root

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,7 +11,7 @@ class PlacesController < ApplicationController
   def create
     @place = current_user.places.create(place_params)
     if @place.valid?
-      redirect_to root_path
+      redirect_to place_path(@place)
     else
       render :new, status: :unprocessable_entity
     end
@@ -31,7 +31,7 @@ class PlacesController < ApplicationController
 
     @place.update_attributes(place_params)
     if @place.valid?
-      redirect_to root_path
+      redirect_to place_path
     else
       render :edit, status: :unprocessable_entity 
     end
